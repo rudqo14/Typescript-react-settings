31ae8a8e05b60f6ca45c956144ce0ae9aed31c4c
KeyoungBae Kim <rudqo14@gmail.com>
2021-05-12 16:47:29 +0900

    Add: 코드스플리팅, 이모션 적용

--- Frontend/layouts/App.tsx
+++ Frontend/layouts/App.tsx
@@ -1,8 +1,9 @@
 import React from 'react';
 import { Switch, Route, Redirect } from 'react-router-dom';
+import loadable from '@loadable/component';
 
-import LogIn from '@pages/LogIn';
-import SignUp from '@pages/SignUp';
+const LogIn = loadable(() => import('@pages/LogIn'));
+const SignUp = loadable(() => import('@pages/SignUp'));
 
 const App = () => {
   return (
