4d6c67bebf5ca95ca6221fc26f4334b7e9b1210b
KeyoungBae Kim <rudqo14@gmail.com>
2021-05-12 16:26:46 +0900

    Add: 초기 폴더 구조 설정 및 라우터 적용

--- Frontend/layouts/App.tsx
+++ Frontend/layouts/App.tsx
@@ -1,8 +1,17 @@
 import React from 'react';
-import { render } from 'react-dom';
+import { Switch, Route, Redirect } from 'react-router-dom';
+
+import LogIn from '@pages/LogIn';
+import SignUp from '@pages/SignUp';
 
 const App = () => {
-  return <div>app.tsx 테스트!</div>;
+  return (
+    <Switch>
+      <Redirect exact path="/" to="/login" />
+      <Route path="/login" component={LogIn} />
+      <Route path="/signup" component={SignUp} />
+    </Switch>
+  );
 };
 
 export default App;
